1609bfbab83ba2cb7aab4a0fac28e5931a435d60
KENNYMOAH <kennyss12345@hotmail.com>
2017-06-22 11:18:19 +0800

    finished but not tested

--- ann_hw3.py
+++ ann_hw3.py
@@ -1,14 +1,23 @@
 try:
-    training_data = open('training_data_2n.txt', 'r')
+    training_data = open('training_data.txt', 'r')
 
 except:
-    print "Can't find training data for two-neuron ADALINE network. Please make sure training_data_2n.txt is under the same directory as this py file."
+    print "Can't find training data. Please make sure training_data.txt is under the same directory as this py file."
     quit()
 
 data = training_data.readlines()
 weight = [1.0, 0.0, 0.0, 1.0]
 bias = [1.0, 1.0]
 learning_rate = 0.1
+a = [0.0, 0.0]
+e = [0.0, 0.0]
 
 for i in range(0, len(data), 4):
-    for j in 
+    for j in range(0, 2):
+        a[j] = float(weight[2 * j]) * float(data[i]) + float(weight[2 * j + 1]) * float(data[i + 1]) + bias[j]
+        e[j] = data[i + j] - a[j]
+    
+    for j in range(0, 2):
+        for k in range(0, 2):
+            weight[2 * j + k] += learning_rate * e[j] * data[i + k]
+        bias[2 * j + k] += learning_rate * e[j]
